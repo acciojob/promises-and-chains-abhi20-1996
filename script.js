f8ffcdcfec4198f45f53de2be9bd7dbf32f51de7
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-03-30 18:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ var promise = new Promise((resolve, reject) => {
   setTimeout(() => {
     var age = ageElement.value;
     var name = nameElement.value;
+     if(!(name && age)){
+		 alert("Please enter valid details")
+	 }
+	  return;
+	  
     if (age < 18) {
       reject(`Oh sorry ${name}. You aren't old enough`);
     } else {
